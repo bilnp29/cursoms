64c955b497afae956d4f521d2277a54c7849c6d8
Bruno Miranda <bilnp29@gmail.com>
2018-12-26 20:04:40 -0300

    Implementado as funções que calcula o subtotal e valor total de produtos.

--- src/main/java/com/miranda/springboot/domain/ItemPedido.java
+++ src/main/java/com/miranda/springboot/domain/ItemPedido.java
@@ -43,6 +43,11 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+	public double getSubtotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
