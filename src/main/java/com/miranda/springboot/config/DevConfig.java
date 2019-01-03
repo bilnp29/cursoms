165a57d69a3399c19d9e4ea7897d83b959a7a305
Bruno Miranda <bilnp29@gmail.com>
2019-01-03 15:52:47 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/miranda/springboot/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.miranda.springboot.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.miranda.springboot.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instanciandoDataBase() throws ParseException {
+
+		if (strategy.equals("create")) {
+			dbService.instaciandoTestDataBase();
+			return true;
+		}
+		return false;
+	}
+}
